5d3680639d61460422dfd20c7ebb9a9ee8cbe9d6
Hardik Makwana <makwanahardik82@gmail.com>
2025-11-28 17:06:17 +0530

    Updated XC - Software

--- Package.swift
+++ Package.swift
@@ -3,15 +3,16 @@
 
 import PackageDescription
 
+/*
 let package = Package(
     name: "Peko-AE-iOS",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Peko-AE-iOS",
-            targets: ["Peko-AE-iOS", "PekoSDK"]
-        ),
+//        .library(
+//            name: "PekoSDK",
+//            targets: ["PekoSDK"]
+//        ),
     ],
     dependencies: [
         .package(url: "https://github.com/PureLayout/PureLayout.git", .upToNextMajor(from: "3.1.9")),
@@ -40,7 +41,43 @@ let package = Package(
 //                .process("Fonts")   // <-- Include font files
 //            ]
         ),
+       // .binaryTarget(name: "PekoSDK", path: "./PekoSDK.xcframework")
+    ]
+)
+*/
+
+
+let package = Package(
+    name: "Peko-AE-iOS",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "Peko-AE-iOS",
+            targets: ["Peko-AE-iOS", "PekoSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/PureLayout/PureLayout.git", .upToNextMajor(from: "3.1.9")),
+        .package(url: "https://github.com/WenchaoD/FSCalendar.git", .upToNextMajor(from: "2.8.4")),
+        .package(url: "https://github.com/Juanpe/SkeletonView.git", .upToNextMajor(from: "1.7.0")),
+        .package(url: "https://github.com/alickbass/CodableFirebase", .upToNextMajor(from: "0.2.2")),
+//        .package(url: "https://github.com/iziz/libPhoneNumber-iOS", .upToNextMajor(from: "1.2.0")),
+            .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.4")),
+            .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.21.2")),
+    ],
+    targets: [
+        .target(
+            name: "Peko-AE-iOS",
+            dependencies: [
+                .product(name: "PureLayout", package: "PureLayout"),
+                .product(name: "FSCalendar", package: "FSCalendar"),
+                .product(name: "SkeletonView", package: "SkeletonView"),
+                .product(name: "CodableFirebase", package: "CodableFirebase"),
+//                .product(name: "libPhoneNumber", package: "libPhoneNumber-iOS"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+            ],
+        ),
         .binaryTarget(name: "PekoSDK", path: "./PekoSDK.xcframework")
     ]
 )
-
--- Sources/Peko-AE-iOS/Peko_AE_iOS.swift
+++ Sources/Peko-AE-iOS/Peko_AE_iOS.swift
@@ -1,3 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+//import 
+import PekoSDK
+import UIKit
 
+public class PekoManager {
+    public init(){
+    
+    }
+    @MainActor public func userLogin(userName:String, password:String, viewController: UIViewController) {
+       
+        let userName = "100000001"
+        let password = "Admin@123"
+       
+        let obj = PekoSDKManager()
+        obj.userLogin(username: userName, password: password, viewController: viewController)
+    }
+}
